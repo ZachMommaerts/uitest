e296a1c689007ba168bf17a4cd411395ee640c78
ZachMommaerts <zmommaerts@gmail.com>
2023-03-01 13:53:58 -0600

    Initial Commit

---
+++ uitest/uitestApp.swift
@@ -0,0 +1,17 @@
+//
+//  uitestApp.swift
+//  uitest
+//
+//  Created by Zach Mommaerts on 3/1/23.
+//
+
+import SwiftUI
+
+@main
+struct uitestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
