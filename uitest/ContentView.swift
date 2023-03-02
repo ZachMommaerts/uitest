16a9d8ae34862d95a04b9a4d61575c33133adba1
ZachMommaerts <zmommaerts@gmail.com>
2023-03-01 18:07:10 -0600

    CWC #4 Challenge

--- uitest/ContentView.swift
+++ uitest/ContentView.swift
@@ -10,17 +10,51 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Text("Hello, world!")
-                .foregroundColor(Color.white)
-                .padding(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
-                .background(Color.green)
+            ZStack {
+                Image("toronto")
+                    .resizable().aspectRatio(contentMode: .fit)
+                    .cornerRadius(10)
+                
+                VStack {
+                    Text("CN Tower")
+                        .foregroundColor(Color.white)
+                        .font(.system(size: 40))
+                    
+                    
+                    Text("Toronto")
+                        .foregroundColor(Color.white)
+                }
+                .padding()
+                .background(Color.black)
                 .cornerRadius(10)
-                .padding(.all)
-                .background(Color.blue)
+                .opacity(0.8)
+            }
+            .padding()
+            
+            ZStack{
+                Image("london")
+                    .resizable().aspectRatio(contentMode: .fit)
+                    .cornerRadius(10)
+                
+                VStack {
+                    Text("Big Ben")
+                        .foregroundColor(Color.white)
+                        .font(.system(size: 40))
+                    
+                    
+                    Text("London")
+                        .foregroundColor(Color.white)
+                }
+                .padding()
+                .background(Color.black)
                 .cornerRadius(10)
+                .opacity(0.8)
+            }
+            .padding()
         }
-        .padding()
     }
+    
+    
 }
 
 struct ContentView_Previews: PreviewProvider {
